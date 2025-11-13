225d77d62ad9241b24a9c6264f881215ddfba997
JeyakumarMoorthy <JeyakumarMoorthy@KANINI-LTP-764>
2025-11-13 14:04:58 +0530

    initial commit

---
+++ banking-app-backend/src/test/java/com/example/banking/app/backend/BankingAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.banking.app.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
